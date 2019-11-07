48316919938b74239c2019782a8663a6552e2809
Ari Kardasis <ari.kardasis@gmail.com>
2019-11-07 08:12:46 -0500

    Initial Commit

---
+++ Lilypad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lilypad
+//
+//  Created by Ari Kardasis on 11/7/19.
+//  Copyright © 2019 Lilypad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
